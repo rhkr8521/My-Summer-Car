10b51ca2a04b4ea37ced18a4e25e557626b68358
LJSoo <jisuu7813@naver.com>
2025-05-30 16:51:41 +0900

    bus

---
+++ src/car/Bus.java
@@ -0,0 +1,25 @@
+package car;
+
+public class Bus extends Car implements AirCon, AutoDrive {
+
+    public Bus(String name) {
+        super(150, 5, 100, 20, name); // speed, fuelEfficiency, fuelCapacity, seatCount, name
+    }
+
+    @Override
+    public void setMode(boolean isOn) {
+        if (isOn) {
+            this.fuelCapacity += 30; // 보조 연료탱크 추가
+        }
+    }
+
+    @Override
+    public void airConSpeak(boolean isOn) {
+        System.out.println(name + ": 에어컨 " + (isOn ? "ON" : "OFF"));
+    }
+
+    @Override
+    public void autoDriveSpeak(boolean isOn) {
+        System.out.println(name + ": 자율주행 " + (isOn ? "ON" : "OFF"));
+    }
+}
